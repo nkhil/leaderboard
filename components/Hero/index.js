3a1b10b801732c0c1423bf8fe5e2e29f7346ebe6
Nikhil Vijayan <nikhil.vijayan@bglgroup.co.uk>
2020-12-10 16:24:06 +0000

    feat: integrate logo and button component to homepage

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,13 +1,33 @@
-import WideContainer from '@components/WideContainer'
+import WideContainer from '@components/WideContainer';
+import Button from '@components/Button';
+import theme from '../../constants/theme';
+import Image from 'next/image';
 
 import styled from 'styled-components';
 
 function Hero({ className }) {
   return (
-    <WideContainer bgColour='#14B1ED'>
+    <WideContainer bgColour={theme.heroColour}>
       <div className={className}>
-        <h2>Leaderboard is a simple API to record scores and manage a leaderboard</h2>
-        <button type="button">Get started</button>
+        <Image
+          src="/images/logo.png"
+          alt="Picture of the author"
+          width={250}
+          height={50}
+        />
+        <h2>Simple API to record scores and manage a leaderboard</h2>
+        <Button
+          text={'Get Started'}
+          link={'/sign-up'}
+          isInternal={true}
+          newWindow={false}
+        />
+        <Button
+          text={'Read the docs'}
+          link={'/documentation'}
+          isInternal={true}
+          newWindow={false}
+        />
       </div>
     </WideContainer>
   );
@@ -16,13 +36,14 @@ function Hero({ className }) {
 export default styled(Hero)`
   max-width: 1000px;
   color: white;
-  padding: 50px;
+  padding: 50px 20px;
   margin: auto;
 
   h2 {
     font-weight: 200;
     font-size: 2.5rem;
     padding: 30px;
+    margin-bottom: 20px;
   }
 
   @media only screen and (min-device-width: 320px) and (max-device-width: 480px) and (-webkit-min-device-pixel-ratio: 2) {
@@ -32,6 +53,7 @@ export default styled(Hero)`
     h2 {
       font-size: 2rem;
       padding: 5px;
+      margin-bottom: 20px;
     }
   }
 `;
